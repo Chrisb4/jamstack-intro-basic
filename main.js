46a1ca1d347cdee12ebc7c71563cc1d897a4202d
Chris Brown <nudgechris@gmail.com>
2020-05-16 22:39:56 -0500

    first jamstack site

---
+++ main.js
@@ -0,0 +1,24 @@
+const listrepos = async username => {
+    const repos = await fetch(
+        `https://api.github.com/users/${username}/repos?type=owner&sort=update`
+    )
+        .then(res => res.json())
+        .catch(error => console.error(error));
+    
+    const markup = repos
+        .map(
+            repo => `
+                <li>
+                    <a href="${repo.html_url}">${repo.name}</a>
+                    (⭐${repo.stargazers_count})
+                </li>
+            `
+        )
+        .join('');
+
+    const content = document.getElementById('content');
+
+    content.innerHTML = `<ul>${markup}</ul>`;
+}
+
+listrepos('Chrisb4');
